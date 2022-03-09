fd3c691da71ec7d9aabe670d1b0b8813c6bef9c5
jieonkiim <jaykimma920@gmail.com>
2022-03-09 16:46:37 +0900

    오늘의집 게시판 연습

---
+++ 6. OHouse_practice/src/posts.js
@@ -0,0 +1,34 @@
+const $postingContainer = document.querySelector(".posting-container");
+
+const getPostTemplate = (post) => {
+  return `
+    <a href="/detail.html?id=${post.id}">
+    <div class="posting-wrapper">
+      <div class="posting-image-container">
+        <img
+          src="${post.image}"
+          alt="게시글 이미지"
+        />
+      </div>
+      <h2 class="">${post.title}</h2>
+      <div class="profile-wrapper">
+        <div class="profile-image-container">
+          <img
+            class="profile-image"
+            src="${post.authorImage}"
+            alt="profile-image"
+          />
+        </div>
+        <span class="profile-nickname">${post.author}</span>
+      </div>
+    </div>
+  `;
+};
+
+const getPostsTemplate = (post) => {
+  return post.map((post) => getPostTemplate(post)).join("");
+};
+
+requestGetPosts().then((post) => {
+  $postingContainer.innerHTML = getPostsTemplate(post);
+});
